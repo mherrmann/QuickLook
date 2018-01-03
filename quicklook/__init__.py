18e6229ba6f4b9334ce90b4dd697a5a479efb27a
Michael Herrmann <michael@herrmann.io>
2018-01-03 19:28:28 +0100

    Fix for fman 0.7.0+

--- quicklook/__init__.py
+++ quicklook/__init__.py
@@ -1,8 +1,9 @@
-from subprocess import Popen
 from fman import DirectoryPaneCommand
+from fman.url import as_human_readable
+from subprocess import Popen
 
 class QuickLook(DirectoryPaneCommand):
 	def __call__(self):
 		file_under_cursor = self.pane.get_file_under_cursor()
 		if file_under_cursor:
-			Popen('qlmanage -p "%s"' % file_under_cursor, shell=True)
+			Popen(['qlmanage', '-p', as_human_readable(file_under_cursor)])
